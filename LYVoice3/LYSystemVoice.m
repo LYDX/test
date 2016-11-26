2a5b0be458a4e6bcb8dccca956e7d543f3dc3938
111 <a111@111deMacBook-Air.local>
2016-11-26 14:16:15 +0800

    测试

---
+++ LYVoice3/LYSystemVoice.m
@@ -0,0 +1,17 @@
+//
+//  LYSystemVoice.m
+//  LYSDK
+//
+//  Created by 111 on 16/11/21.
+//  Copyright © 2016年 111. All rights reserved.
+//
+
+#import "LYSystemVoice.h"
+
+@implementation LYSystemVoice
+
+- (void)delete{
+    NSLog(@"delete");
+
+}
+@end
