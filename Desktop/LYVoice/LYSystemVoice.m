002b339576ac78f082d9f5c481c86fa81da784f1
111 <a111@111deMacBook-Air.local>
2016-11-26 10:39:47 +0800

    调用系统声音

---
+++ Desktop/LYVoice/LYSystemVoice.m
@@ -0,0 +1,13 @@
+//
+//  LYSystemVoice.m
+//  LYSDK
+//
+//  Created by 111 on 16/11/21.
+//  Copyright © 2016年 111. All rights reserved.
+//
+
+#import "LYSystemVoice.h"
+
+@implementation LYSystemVoice
+
+@end
